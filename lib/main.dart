a5eeb3e438d8a396e557f4f96f1fca67c1020618
ersinsonmez026-glitch <ersinsonmez026@gmail.com>
2026-09-25 10:56:22 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,29 +12,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Ezan Saati',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Ezan Saati Ana Sayfa'),
+      home: const MyHomePage(
+        title: 'Ezan Saati Ana Sayfa',
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({
+    super.key,
+    required this.title,
+  });
 
   final String title;
 
   @override
-  State createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State {
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor:
+            Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: const Center(
